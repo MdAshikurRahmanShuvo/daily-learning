2407f50d8619d3a024b6c8022f617c1a9dc4ff89
MdAshikurRahmanShuvo <shuvo15-5633@diu.edu.bd.com>
2026-05-17 22:07:44 +0600

    Added First Vb.net Project Data_Retrieved_only

---
+++ VB.NET/Project/Data_Retreived_only/Data_Retreived_only/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        CrystalReportViewer1.ReportSource = Nothing
+        Me.WindowState = FormWindowState.Maximized
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim con As New SqlConnection("Server=.;Database=BNBDAT;User Id=sa;Password=1122;")
+        Dim da As New SqlDataAdapter("SELECT * FROM GLAMF", con)
+        Dim dt As New DataTable()
+
+        da.Fill(dt)
+
+        Dim rpt As New CrystalReport2()
+        rpt.SetDataSource(dt)
+
+        CrystalReportViewer1.ReportSource = rpt
+        CrystalReportViewer1.Refresh()
+
+    End Sub
+
+End Class
